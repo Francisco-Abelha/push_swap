68f4bec98e5fe98411cefc78fe973f73b534bebc
francisco <francisco.abelha@icloud.com>
2026-01-01 23:00:32 +0000

    created stack from args and printed it

--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,14 +6,29 @@
 /*   By: fgoncal2 <fgoncal2@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/01 18:15:17 by fgoncal2          #+#    #+#             */
-/*   Updated: 2026/01/01 18:22:57 by fgoncal2         ###   ########.fr       */
+/*   Updated: 2026/01/01 22:48:43 by fgoncal2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int	main()
+int	main(int argc, char **argv)
 {
-	ft_printf("%s\n", "shgahf");
-	return (0);
+	t_list	*stack_a;
+	//t_list	*stack_b;
+	int		i;
+
+	if (argc < 2)
+		return (0);
+	i = 1;
+	stack_a = ft_lstnew(argv[i]);
+	//stack_b = NULL;
+	i++;
+	while (argv[i])
+	{
+		ft_lstadd_back(&stack_a, ft_lstnew(argv[i]));
+		i++;
+	}
+	ft_lstiter(stack_a, print_str);
+
 }
